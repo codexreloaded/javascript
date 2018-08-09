20ae01cda6837b69a912c344f545ea5348066ead
Aldy Apriansyah <32292702+codexreloaded@users.noreply.github.com>
2018-08-09 21:22:57 +0700

    Update recent.js

--- recent.js
+++ recent.js
@@ -1,5 +1,5 @@
 //<![CDATA[
-var homePage = "//www.codexreloadedgames.tk",
+var homePage = "//www.codexreloaded.tk",
 numPosts = 13;
 function recentPosts(a){if(document.getElementById("recent-posts")){var e=a.feed.entry,title,link,content="",ct=document.getElementById("recent-posts");for(var i=0;i<numPosts;i++){for(var j=0;j<numPosts;j++){if(e[i].link[j].rel=="alternate"){link=e[i].link[j].href;break}}var title=e[i].title.$t;content+='<li class="recent-posts"><a href="'+link+'" title="'+title+'" target="_blank" rel="nofollow">'+title+'</a></li>'}ct.innerHTML=content}}var rcp=document.createElement('script');rcp.src=homePage+'/feeds/posts/summary?alt=json-in-script&orderby=published&max-results='+numPosts+'&callback=recentPosts';document.getElementsByTagName('head')[0].appendChild(rcp);
 //]]>
