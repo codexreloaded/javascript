727c81bfe094738d2aeda46d8b9699ba25c59a02
Aldy Apriansyah <32292702+codexreloaded@users.noreply.github.com>
2018-09-19 20:17:10 +0700

    Update ajaxsearchform.js

--- ajaxsearchform.js
+++ ajaxsearchform.js
@@ -1,3 +1,284 @@
 //<![CDATA[
-jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});var searchFormConfig={summaryPost:true,resultThumbnail:true,fallbackThumb:"http://1.bp.blogspot.com/-41xX9MKBr-w/U1Tt0T8gNRI/AAAAAAAABZw/SocPS0qeq1k/s50-c/Noim.png",thumbSize:50,summaryLength:55};(function(h){var f=h("#ajax-search-form"),g=f.find(":text");config=searchFormConfig;f.append('<div id="search-result"></div>');var e=h("#search-result");f.on("submit",function(){var a=g.val();e.slideDown(500,"easeOutBounce").html("Loading...");h.ajax({url:"https://codexreloadedgames.blogspot.com/feeds/posts/summary?alt=json-in-script&q="+a+"&max-results=9999",type:"get",dataType:"jsonp",success:function(b){var c=b.feed.entry,j,r,d,i,u="";if(c!==undefined){u="<h4>Search results for keyword &quot;"+a+"&quot;</h4>";u+='<a class="close" href="/">&times;</a><ul>';for(var s=0;s<c.length;s++){var v=new RegExp(a,"ig"),d=c[s].title.$t.replace(v,"<mark>"+a+"</mark>");for(var t=0;t<c[s].link.length;t++){if(c[s].link[t].rel=="alternate"){i=c[s].link[t].href}}if(config.summaryPost===true){if("content" in c[s]){j=c[s].content.$t}else{if("summary" in c[s]){j=c[s].summary.$t}else{j=""}}}if(config.resultThumbnail===true){if("media$thumbnail" in c[s]){r=c[s].media$thumbnail.url.replace(/\/s[0-9]+\-c/g,"/s"+config.thumbSize+"-c")}else{r=config.fallbackThumb}}j=j.replace(/<\S[^>]*>/g,"");if(j.length>config.summaryLength){j=j.substring(0,config.summaryLength)+"..."}j=j.replace(v,"<mark>"+a+"</mark>");u+='<li><a href="'+i+'" target="_blank"><span><img src="'+r+'" alt="" /></span><h5>'+d+"</h5></a><p>"+j+"</p></li>"}u+="</ul>";e.html(u)}else{e.html('<a class="close" href="/">&times;</a><strong>No result!</strong>')}},error:function(){e.html('<a class="close" href="/">&times;</a><strong>Error loading feed.</strong>')}});return false});f.on("click",".close",function(){e.slideUp(500,"easeInExpo");return false})})(jQuery);
+jQuery.easing.jswing = jQuery.easing.swing;
+jQuery.extend(jQuery.easing, {
+    def: "easeOutQuad",
+    swing: function (x, t, b, c, d) {
+        return jQuery.easing[jQuery.easing.def](x, t, b, c, d)
+    },
+    easeInQuad: function (x, t, b, c, d) {
+        return c * (t /= d) * t + b
+    },
+    easeOutQuad: function (x, t, b, c, d) {
+        return -c * (t /= d) * (t - 2) + b
+    },
+    easeInOutQuad: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) {
+            return c / 2 * t * t + b
+        }
+        return -c / 2 * ((--t) * (t - 2) - 1) + b
+    },
+    easeInCubic: function (x, t, b, c, d) {
+        return c * (t /= d) * t * t + b
+    },
+    easeOutCubic: function (x, t, b, c, d) {
+        return c * ((t = t / d - 1) * t * t + 1) + b
+    },
+    easeInOutCubic: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) {
+            return c / 2 * t * t * t + b
+        }
+        return c / 2 * ((t -= 2) * t * t + 2) + b
+    },
+    easeInQuart: function (x, t, b, c, d) {
+        return c * (t /= d) * t * t * t + b
+    },
+    easeOutQuart: function (x, t, b, c, d) {
+        return -c * ((t = t / d - 1) * t * t * t - 1) + b
+    },
+    easeInOutQuart: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) {
+            return c / 2 * t * t * t * t + b
+        }
+        return -c / 2 * ((t -= 2) * t * t * t - 2) + b
+    },
+    easeInQuint: function (x, t, b, c, d) {
+        return c * (t /= d) * t * t * t * t + b
+    },
+    easeOutQuint: function (x, t, b, c, d) {
+        return c * ((t = t / d - 1) * t * t * t * t + 1) + b
+    },
+    easeInOutQuint: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) {
+            return c / 2 * t * t * t * t * t + b
+        }
+        return c / 2 * ((t -= 2) * t * t * t * t + 2) + b
+    },
+    easeInSine: function (x, t, b, c, d) {
+        return -c * Math.cos(t / d * (Math.PI / 2)) + c + b
+    },
+    easeOutSine: function (x, t, b, c, d) {
+        return c * Math.sin(t / d * (Math.PI / 2)) + b
+    },
+    easeInOutSine: function (x, t, b, c, d) {
+        return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b
+    },
+    easeInExpo: function (x, t, b, c, d) {
+        return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b
+    },
+    easeOutExpo: function (x, t, b, c, d) {
+        return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b
+    },
+    easeInOutExpo: function (x, t, b, c, d) {
+        if (t == 0) {
+            return b
+        }
+        if (t == d) {
+            return b + c
+        }
+        if ((t /= d / 2) < 1) {
+            return c / 2 * Math.pow(2, 10 * (t - 1)) + b
+        }
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b
+    },
+    easeInCirc: function (x, t, b, c, d) {
+        return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b
+    },
+    easeOutCirc: function (x, t, b, c, d) {
+        return c * Math.sqrt(1 - (t = t / d - 1) * t) + b
+    },
+    easeInOutCirc: function (x, t, b, c, d) {
+        if ((t /= d / 2) < 1) {
+            return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b
+        }
+        return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b
+    },
+    easeInElastic: function (x, t, b, c, d) {
+        var s = 1.70158;
+        var p = 0;
+        var a = c;
+        if (t == 0) {
+            return b
+        }
+        if ((t /= d) == 1) {
+            return b + c
+        }
+        if (!p) {
+            p = d * 0.3
+        }
+        if (a < Math.abs(c)) {
+            a = c;
+            var s = p / 4
+        } else {
+            var s = p / (2 * Math.PI) * Math.asin(c / a)
+        }
+        return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b
+    },
+    easeOutElastic: function (x, t, b, c, d) {
+        var s = 1.70158;
+        var p = 0;
+        var a = c;
+        if (t == 0) {
+            return b
+        }
+        if ((t /= d) == 1) {
+            return b + c
+        }
+        if (!p) {
+            p = d * 0.3
+        }
+        if (a < Math.abs(c)) {
+            a = c;
+            var s = p / 4
+        } else {
+            var s = p / (2 * Math.PI) * Math.asin(c / a)
+        }
+        return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b
+    },
+    easeInOutElastic: function (x, t, b, c, d) {
+        var s = 1.70158;
+        var p = 0;
+        var a = c;
+        if (t == 0) {
+            return b
+        }
+        if ((t /= d / 2) == 2) {
+            return b + c
+        }
+        if (!p) {
+            p = d * (0.3 * 1.5)
+        }
+        if (a < Math.abs(c)) {
+            a = c;
+            var s = p / 4
+        } else {
+            var s = p / (2 * Math.PI) * Math.asin(c / a)
+        }
+        if (t < 1) {
+            return -0.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b
+        }
+        return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * 0.5 + c + b
+    },
+    easeInBack: function (x, t, b, c, d, s) {
+        if (s == undefined) {
+            s = 1.70158
+        }
+        return c * (t /= d) * t * ((s + 1) * t - s) + b
+    },
+    easeOutBack: function (x, t, b, c, d, s) {
+        if (s == undefined) {
+            s = 1.70158
+        }
+        return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b
+    },
+    easeInOutBack: function (x, t, b, c, d, s) {
+        if (s == undefined) {
+            s = 1.70158
+        }
+        if ((t /= d / 2) < 1) {
+            return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b
+        }
+        return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b
+    },
+    easeInBounce: function (x, t, b, c, d) {
+        return c - jQuery.easing.easeOutBounce(x, d - t, 0, c, d) + b
+    },
+    easeOutBounce: function (x, t, b, c, d) {
+        if ((t /= d) < (1 / 2.75)) {
+            return c * (7.5625 * t * t) + b
+        } else {
+            if (t < (2 / 2.75)) {
+                return c * (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75) + b
+            } else {
+                if (t < (2.5 / 2.75)) {
+                    return c * (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375) + b
+                } else {
+                    return c * (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375) + b
+                }
+            }
+        }
+    },
+    easeInOutBounce: function (x, t, b, c, d) {
+        if (t < d / 2) {
+            return jQuery.easing.easeInBounce(x, t * 2, 0, c, d) * 0.5 + b
+        }
+        return jQuery.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * 0.5 + c * 0.5 + b
+    }
+});
+/* Search*/
+var searchFormConfig = {
+    summaryPost: true,
+    resultThumbnail: true,
+    fallbackThumb: "http://1.bp.blogspot.com/-41xX9MKBr-w/U1Tt0T8gNRI/AAAAAAAABZw/SocPS0qeq1k/s50-c/Noim.png",
+    thumbSize: 50,
+    summaryLength: 100
+};
+(function (c) {
+    var a = c("#ajax-search-form"),
+        d = a.find(":text");
+    config = searchFormConfig;
+    a.append('<div id="search-result"></div>');
+    var b = c("#search-result");
+    a.on("submit", function () {
+        var e = d.val();
+        b.slideDown(500, "easeOutBounce").html("Loading...");
+        c.ajax({
+            url: "https://codexreloadedgames.blogspot.com//feeds/posts/summary?alt=json-in-script&q=" + e + "&max-results=9999",
+            type: "get",
+            dataType: "jsonp",
+            success: function (q) {
+                var p = q.feed.entry,
+                    m, l, o, n, g = "";
+                if (p !== undefined) {
+                    g = "<h4>Search results for keyword &quot;" + e + "&quot;</h4>";
+                    g += '<a class="close" href="/">&times;</a><ul>';
+                    for (var k = 0; k < p.length; k++) {
+                        var f = new RegExp(e, "ig"),
+                            o = p[k].title.$t.replace(f, "<mark>" + e + "</mark>");
+                        for (var h = 0; h < p[k].link.length; h++) {
+                            if (p[k].link[h].rel == "alternate") {
+                                n = p[k].link[h].href
+                            }
+                        }
+                        if (config.summaryPost === true) {
+                            if ("content" in p[k]) {
+                                m = p[k].content.$t
+                            } else {
+                                if ("summary" in p[k]) {
+                                    m = p[k].summary.$t
+                                } else {
+                                    m = ""
+                                }
+                            }
+                        }
+                        if (config.resultThumbnail === true) {
+                            if ("media$thumbnail" in p[k]) {
+                                l = p[k].media$thumbnail.url.replace(/\/s[0-9]+\-c/g, "/s" + config.thumbSize + "-c")
+                            } else {
+                                l = config.fallbackThumb
+                            }
+                        }
+                        m = m.replace(/<\S[^>]*>/g, "");
+                        if (m.length > config.summaryLength) {
+                            m = m.substring(0, config.summaryLength) + "..."
+                        }
+                        m = m.replace(f, "<mark>" + e + "</mark>");
+                        g += '<li><a href="' + n + '" target="_blank"><span><img src="' + l + '" alt="" /></span><h5>' + o + "</h5></a><p>" + m + "</p></li>"
+                    }
+                    g += "</ul>";
+                    b.html(g)
+                } else {
+                    b.html('<a class="close" href="/">&times;</a><strong>No result!</strong>')
+                }
+            },
+            error: function () {
+                b.html('<a class="close" href="/">&times;</a><strong>Error loading feed.</strong>')
+            }
+        });
+        return false
+    });
+    a.on("click", ".close", function () {
+        b.slideUp(500, "easeInExpo");
+        return false
+    })
+})(jQuery);
 //]]>
